e1e8fc94cee69a6ebc8bf60587148b71d361bac2
neon_home <niki.kudrin@gmail.com>
2021-04-09 19:23:00 +0300

    thread function to print a number without if / switch / stream api

--- src/main/java/NumberPrinter.java
+++ src/main/java/NumberPrinter.java
@@ -1,14 +1,50 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class NumberPrinter {
+    private AtomicInteger counter = new AtomicInteger();
+    private ArrayList<Integer> sequence = new ArrayList<>();
+
+    private class CounterExecutor implements Callable {
+
+        @Override
+        public Object call() throws Exception {
+            sequence.add(counter.incrementAndGet());
+            return null;
+        }
+    }
+
+    public void printNumberUsingThread(final int maxNumber) throws InterruptedException {
+        assert maxNumber >= 1;
+
+        var poolExecutor = Executors.newSingleThreadExecutor();
+
+        var threads = new CounterExecutor[maxNumber];
+        Arrays.fill(threads, new CounterExecutor());
+
+        List<Callable<Object>> list = Arrays.asList(threads);
+
+        poolExecutor.invokeAll(list);
+        poolExecutor.shutdown();
+        poolExecutor.awaitTermination(120, TimeUnit.SECONDS);
+
+        System.out.println(sequence);
+    }
+
     /**
-     * Recursive function to print number without loop, if or switch-case comparison
-     * Usage of stream API also prohibited
+     * Recursive function to print number without loop, if or switch-case comparison, usage of stream API also prohibited
+     * NOTE: WILL FAIL on big numbers ~ a few thousands
      *
      * @param maxNumber - Maximum number to print
      * @return
      */
-    public static int printNumbers(final int maxNumber) {
-
-        var number = (maxNumber <= 0) ? -1 : printNumbers(maxNumber - 1);
+    public static int printNumbersRecursive(final int maxNumber) {
+        var number = (maxNumber <= 0) ? -1 : printNumbersRecursive(maxNumber - 1);
         System.out.println(maxNumber);
         return maxNumber;
     }
--- src/test/java/NumberPrinterTest.java
+++ src/test/java/NumberPrinterTest.java
@@ -2,12 +2,33 @@ import org.junit.jupiter.api.Test;
 
 public class NumberPrinterTest {
     @Test
-    void maxIntValueTest() {
-        NumberPrinter.printNumbers(Integer.MAX_VALUE);
+    void smallValueTest() throws InterruptedException {
+        new NumberPrinter().printNumberUsingThread(100_000);
     }
 
     @Test
-    void negativeValueTest() {
-        NumberPrinter.printNumbers(-2);
+    void hugeIntValueTest() throws InterruptedException {
+        new NumberPrinter().printNumberUsingThread(1_000_000);
+    }
+
+    @Test
+    void maxIntValueTest() throws InterruptedException {
+        new NumberPrinter().printNumberUsingThread(Integer.MAX_VALUE);
+    }
+
+    @Test
+    void negativeValueTest() throws InterruptedException {
+        new NumberPrinter().printNumberUsingThread(-3);
+    }
+
+    // Recursive solution: WILL NOT WORK on big numbers (over ~ 15_000)
+    @Test
+    void recursive_smallIntNumberTest() {
+        NumberPrinter.printNumbersRecursive(8);
+    }
+
+    @Test
+    void recursive_hugeIntValueTest() {
+        NumberPrinter.printNumbersRecursive(20_000);
     }
 }
